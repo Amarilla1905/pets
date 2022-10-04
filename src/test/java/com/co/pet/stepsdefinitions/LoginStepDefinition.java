6685730c48537657c9c5d7f41991fac9c0f3780f
Sara <saracastanom19@gmail.com>
2022-10-04 09:58:47 -0500

    creacion login.feature,runner y stepdefinition

---
+++ src/test/java/com/co/pet/stepsdefinitions/LoginStepDefinition.java
@@ -0,0 +1,25 @@
+package com.co.pet.stepsdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginStepDefinition {
+    @Given("the user is on the page")
+    public void theUserIsOnThePage() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("he user enter credentials")
+    public void heUserEnterCredentials() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("successful entry")
+    public void successfulEntry() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
---
+++ src/test/resources/features/login.feature
@@ -0,0 +1,8 @@
+Feature: Login
+  I as service user, need login
+
+  Scenario: Login Successful
+    Given the user is on the page
+    When he user enter credentials
+    Then successful entry
+
